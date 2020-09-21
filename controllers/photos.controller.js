d54c7100edd0f05a67ac9f8a442af22ddb957516
Agata Pacholczyk <agataa.pacholczyk@gmail.com>
2020-09-21 18:42:42 +0200

    add task [25.5]

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -1,4 +1,5 @@
 const Photo = require('../models/photo.model');
+const Voter = require('../models/Voter.model');
 
 /****** SUBMIT PHOTO ********/
 
@@ -11,9 +12,24 @@ exports.add = async (req, res) => {
     if(title && author && email && file) { // if fields are not empty...
 
       const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
+
+      const fileExt = fileName.split('.').slice(-1)[0];
+      const correctWord = new RegExp (/[^a-zA-Z]*$/);
+      const correctTitle = correctWord.test(title);
+      const correctAuthor = correctWord.test(author);
+
+      const checkEmail = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+      const correctEmail = checkEmail.test(email);
+
+      if (fileExt == 'jpg' || fileExt == 'gif' || fileExt == 'png' && correctTitle == true && correctAuthor == true && correctEmail == true) {
+
       const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
       await newPhoto.save(); // ...save new photo in DB
       res.json(newPhoto);
+
+      } else {
+        throw new Error('Wrong file!')
+      }
 
     } else {
       throw new Error('Wrong input!');
@@ -43,11 +59,27 @@ exports.vote = async (req, res) => {
 
   try {
     const photoToUpdate = await Photo.findOne({ _id: req.params.id });
+    const voter = await Voter.findOne({ user: req.clientIp });
     if(!photoToUpdate) res.status(404).json({ message: 'Not found' });
     else {
-      photoToUpdate.votes++;
-      photoToUpdate.save();
-      res.send({ message: 'OK' });
+      if (voter) {
+        if (voter.votes.includes(photoToUpdate._id)){
+          res.status(500).json(err);
+        }
+        else {
+          voter.votes.push(photoToUpdate._id);
+          photoToUpdate.votes++;
+          photoToUpdate.save();
+          res.send({ message: 'OK' });
+        }
+      } else {
+        const newVoter = new Voter({ user: req.clientIp, votes: [photoToUpdate._id]});
+        await newVoter.save();
+        photoToUpdate.votes++;
+        photoToUpdate.save();
+        res.send({ message: 'OK' });
+      }
+      
     }
   } catch(err) {
     res.status(500).json(err);
---
+++ models/Voter.model.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const votersSchema = new mongoose.Schema({
+  user: { type: String, required: true },
+  votes: { type: Array, required: true },
+ 
+});
+
+module.exports = mongoose.model('Voter', votersSchema);
